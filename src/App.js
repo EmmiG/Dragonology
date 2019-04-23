ce53111a7dd5c4e5bfc2a8f10a9858abf0cd9ea1
Dragoness390 <cera11@hotmail.se>
2019-04-23 16:16:01 +0200

    First commit, added project to github

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -17,7 +19,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React...
           </a>
         </header>
       </div>
