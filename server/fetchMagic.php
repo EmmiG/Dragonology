14f50a1a93484a88b0ccd76fbf3470a8e900aa82
Dragoness390 <cera11@hotmail.se>
2019-10-18 21:46:29 +0200

    Fixed: Database: I am now done with taking in the data on the tables: dragonelements, questions and answers. Fixed: fetchMagic.php: First I thought of the solution in another way to make the magic id in the right order that I want 62,1,2,3,4,63 by from the last commit just take in the 62 and 63 magic id, but it didnt work instead it fetched out the result in the order: 1,2,3,4,5,62,63 which wasnt how I wanted it. Instead I searched for an answer in stack over flow and noticed that i could use ORDER BY CASE id, an both took in WHEN and THEN and which order I wanted the ids to be 62,1,2,3,4,63. I changed the limit to be 7 instead of 5, it now fetches like I want it to. I have forgot to say in the commits that I have also updated the comment which explain what the code does. Summery: Fixed: Fetches all the Water dragon magic ids like I want it to.

--- server/fetchMagic.php
+++ server/fetchMagic.php
@@ -1,10 +1,21 @@
 <?php
     /* 
-    * This file fetches the id = 1,2,3,4 and 5 which stands for the Water dragon,
-    * just in case I took limit on just 5 so the server won´t fetch more than 5.
+    * This file fetches the id = 62,1,2,3,4,63 and 5 which stands for the Water dragon,
+    * the id 62 and 63 are the Earth Elemental = Main Element text and Water Elemental = Minor Element text
+    * I took in WHEN and THEN so that the server know which order I want the ids to be,
+    * I took a limit on just 7 so the server won´t fetch more than 7.
     */
     include_once 'database.php';
-    $statement = $pdo->prepare("SELECT * FROM magics WHERE id IN (1,2,3,4,5) LIMIT 5");
+    $statement = $pdo->prepare("SELECT * FROM magics ORDER BY CASE id
+  WHEN 62 THEN 1
+  WHEN 1 THEN 2
+  WHEN 2 THEN 3
+  WHEN 3 THEN 4
+  WHEN 4 THEN 5
+  WHEN 63 THEN 6
+  ELSE 7
+END
+LIMIT 7");
     $statement->execute();
     $data = $statement->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($data, JSON_PRETTY_PRINT);
